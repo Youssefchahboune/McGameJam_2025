123064fdadefb0c9b62cc22087461a05f61cd5f3
Youssef Chaboune <Chahbouney2000@outlook.fr>
2025-01-11 22:32:12 -0500

    Revert "added FMOD"
    This reverts commit 5566391a39e46cfec40940b77bbcc94dce181caf.

--- McGameJam2025/Assets/Player Assets/PlayerController.cs
+++ McGameJam2025/Assets/Player Assets/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     private float originalPlayerSpeed;
     private float originalPlayerGravity;
     public Animator animator;
+    private AudioSource[] audioSources;
 
     // jump variables
     public float jumpForce = 5f;
@@ -45,6 +46,7 @@ public class PlayerController : MonoBehaviour
         originalPlayerSpeed = playerSpeed;
         originalPlayerGravity = playerRb.gravityScale;
         dashAfterEffectParticles = dashAfterEffectParticlesGO.GetComponent<ParticleSystem>();
+        audioSources = GetComponents<AudioSource>();
     }
 
     // Update is called once per frame
@@ -84,6 +86,10 @@ public class PlayerController : MonoBehaviour
             {
             
                 animator.SetBool("isRunning", true);
+                if (!audioSources[0].isPlaying)
+                {
+                    audioSources[0].Play();
+                }
 
             }
 
@@ -101,6 +107,10 @@ public class PlayerController : MonoBehaviour
             {
 
                 animator.SetBool("isRunning", true);
+                if (!audioSources[0].isPlaying)
+                {
+                    audioSources[0].Play();
+                }
 
             }
             
@@ -112,6 +122,7 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.velocity = new Vector2(0, playerRb.velocity.y);
             animator.SetBool("isRunning", false);
+            audioSources[0].Stop();
         }
     }
 
@@ -158,7 +169,7 @@ public class PlayerController : MonoBehaviour
     {
         playerRb.velocity = Vector2.zero;
         animator.SetBool("isRunning", false);
-        animator.SetBool("isJumping", false);
+        animator.SetBool("isjumping", false);
         playerRb.gravityScale = 0f;
 
         yield return new WaitForSeconds(timeBeforeDashing);
